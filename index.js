60e9af55568a47159fded231b61561c6d061cf28
Demon000 <demonsingur@gmail.com>
2018-07-01 10:23:34 +0300

    webssh: configure sessions

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const config = require('./config');
 
 const express = require('express');
+const session = require('express-session');
+
 const server = express();
 
 server.use(express.static('public'));
+server.use(session({
+	saveUninitialized: false,
+	secret: config.secret,
+	unset: 'destroy'
+}));
 
 server.listen(config.port);
