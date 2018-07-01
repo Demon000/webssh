d27b4df42db4d15e9918daf99f54449d6174cc06
Demon000 <demonsingur@gmail.com>
2018-07-01 14:10:03 +0300

    wessh: rip out express sessions support
    Each terminal will have one socket connection, and data won't be shared in-between sockets. There's no reason to use sessions.

--- index.js
+++ index.js
@@ -3,20 +3,11 @@ const Config = require('./config');
 const Http = require('http');
 
 const Express = require('express');
-const Session = require('express-session');
 const SocketIO = require('socket.io');
 
 const server = Express();
 
 server.use(Express.static('public'));
-
-const session = Session({
-    resave: false,
-    saveUninitialized: false,
-    secret: Config.secret,
-    unset: 'destroy'
-});
-server.use(session);
 
 const httpServer = Http.Server(server);
 const socket = SocketIO(httpServer, {
