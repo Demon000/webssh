c0d95e2644f74f7fb991d44373f7155a4d3e2340
Demon000 <demonsingur@gmail.com>
2018-07-15 11:48:53 +0300

    webssh: use express session and make it available in socket io

--- index.js
+++ index.js
@@ -5,14 +5,26 @@ const Http = require('http');
 const Express = require('express');
 const SocketIO = require('socket.io');
 const SSHSocket = require('./SSHSocket');
+const Session = require('express-session');
 
 const app = Express();
 
 app.use(Express.static('public'));
 
+const session = Session({
+  resave: false,
+  saveUninitialized: false,
+  secret: Config.secret,
+  unset: 'destroy'
+});
+app.use(session);
+
 const server = Http.Server(app);
 const io = SocketIO(server, {
     serveClient: false
+});
+io.use((socket, next) => {
+    session(socket.request, socket.request.res, next);
 });
 io.on('connect', function(socket) {
 	socket.on('ssh:connect', SSHSocket.bind(this, socket));
