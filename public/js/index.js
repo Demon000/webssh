f6b854b528022f3975b5ec3cb165b387c7c1dcd8
Demon000 <demonsingur@gmail.com>
2018-07-17 22:06:13 +0300

    webssh: wrap index logic in an anonymous function

--- public/js/index.js
+++ public/js/index.js
@@ -1,50 +1,52 @@
-function setNotes(state) {
-    var progressNote = document.querySelector('#progress-note');
-    var errorNote  = document.querySelector('#error-note');
-    switch(state) {
-    case 'progress':
-        progressNote.classList.add('visible');
-        errorNote.classList.remove('visible');
-        break;
-    case 'error':
-        progressNote.classList.remove('visible');
-        errorNote.classList.add('visible');
-        break;
-    default:
-        progressNote.classList.remove('visible');
-        errorNote.classList.remove('visible');
-        break;
-    }
-}
-
-function doAuth() {
-    var serverInput = document.querySelector('#server');
-    var usernameInput = document.querySelector('#username');
-    var passwordInput = document.querySelector('#password');
-
-    var auth = {
-        server: serverInput.value,
-        username: usernameInput.value,
-        password: passwordInput.value
-    };
-
-    setNotes('progress');
-
-    superagent
-    .post('/auth')
-    .send(auth)
-    .then(function(response) {
-        var data = response.body;
-        if (data.success) {
-            location.reload();
-            setNotes();
-        } else {
-            setNotes('error');
+(function() {
+    function setNotes(state) {
+        var progressNote = document.querySelector('#progress-note');
+        var errorNote  = document.querySelector('#error-note');
+        switch(state) {
+        case 'progress':
+            progressNote.classList.add('visible');
+            errorNote.classList.remove('visible');
+            break;
+        case 'error':
+            progressNote.classList.remove('visible');
+            errorNote.classList.add('visible');
+            break;
+        default:
+            progressNote.classList.remove('visible');
+            errorNote.classList.remove('visible');
+            break;
         }
-    });
-}
+    }
 
-var connectButton = document.querySelector('#connect');
-if (connectButton) {
-    connectButton.addEventListener('click', doAuth);
-}
+    function doAuth() {
+        var serverInput = document.querySelector('#server');
+        var usernameInput = document.querySelector('#username');
+        var passwordInput = document.querySelector('#password');
+
+        var auth = {
+            server: serverInput.value,
+            username: usernameInput.value,
+            password: passwordInput.value
+        };
+
+        setNotes('progress');
+
+        superagent
+        .post('/auth')
+        .send(auth)
+        .then(function(response) {
+            var data = response.body;
+            if (data.success) {
+                location.reload();
+                setNotes();
+            } else {
+                setNotes('error');
+            }
+        });
+    }
+
+    var connectButton = document.querySelector('#connect');
+    if (connectButton) {
+        connectButton.addEventListener('click', doAuth);
+    }
+})();
