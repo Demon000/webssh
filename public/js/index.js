a8666c5af3f71822750c9fbb7f40ed02c94d0c1a
Demon000 <demonsingur@gmail.com>
2018-07-17 14:16:37 +0300

    webssh: provide some info when connecting

--- public/js/index.js
+++ public/js/index.js
@@ -3,17 +3,46 @@ var usernameInput = document.querySelector('#username');
 var passwordInput = document.querySelector('#password');
 var connectButton = document.querySelector('#connect');
 
-connectButton.addEventListener('click', function() {
+function setNotes(state) {
+    var progressNote = document.querySelector('#progress-note');
+    var errorNote  = document.querySelector('#error-note');
+    switch(state) {
+    case 'progress':
+        progressNote.classList.add('visible');
+        errorNote.classList.remove('visible');
+        break;
+    case 'error':
+        progressNote.classList.remove('visible');
+        errorNote.classList.add('visible');
+        break;
+    default:
+        progressNote.classList.remove('visible');
+        errorNote.classList.remove('visible');
+        break;
+    }
+}
+
+function doAuth() {
     var auth = {
         server: serverInput.value,
         username: usernameInput.value,
         password: passwordInput.value
     };
 
+    setNotes('progress');
+
     superagent
     .post('/auth')
     .send(auth)
     .then(function(response) {
-        console.log(response.body);
+        var data = response.body;
+        if (data.success) {
+            location.reload();
+            setNotes();
+        } else {
+            setNotes('error');
+        }
     });
-});
+}
+
+connectButton.addEventListener('click', doAuth);
