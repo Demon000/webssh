0c366252a0acbbbdc78a9f28551099a9620f8c86
Demon000 <demonsingur@gmail.com>
2018-07-17 23:17:15 +0300

    webssh: add an activity icon and test it

---
+++ public/js/terminal.js
@@ -0,0 +1,29 @@
+(function() {
+    var handleActivity = false;
+    function setActivity(status) {
+        if (!handleActivity) {
+            return;
+        }
+
+        var favicon = document.querySelector('#favicon');
+        if (status) {
+            favicon.href = '/assets/activity.png';
+        } else {
+            favicon.href = '';
+        }
+    }
+
+    function handleVisibilityChange() {
+        if (document.hidden) {
+            handleActivity = true;
+        } else  {
+            setActivity(false);
+            handleActivity = false;
+        }
+    }
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    setTimeout(function() {
+        setActivity(true);
+    }, 1000);
+})();
