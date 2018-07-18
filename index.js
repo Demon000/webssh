bd8b80ae3a12476a46c2b307872d23814096d986
Demon000 <demonsingur@gmail.com>
2018-07-18 08:05:27 +0300

    Revert "webssh: use file-based sesstion storage so I don't need to login on every restart"
    This reverts commit fa15d0762ce1d7fc6fa33676d960637072e898cd.

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const Config = require('./config');
 const Express = require('express');
 const Server = require('http').Server;
 const Session = require('express-session');
-const FileStore = require('session-file-store')(Session);
 const SocketIO = require('socket.io');
 const SSH = require('./SSHWrapper');
 
@@ -18,9 +17,6 @@ app.use(Express.json());
 app.set('view engine', 'ejs');
 
 const session = Session({
-    store: new FileStore({
-        ttl: 86400
-    }),
     resave: false,
     saveUninitialized: false,
     secret: Config.secret,
