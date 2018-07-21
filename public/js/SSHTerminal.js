da970cb1bff3d7679076148d761e64e31628879a
Demon000 <demonsingur@gmail.com>
2018-07-21 11:04:52 +0300

    webssh: only attach after the ssh tunnel is established

--- public/js/SSHTerminal.js
+++ public/js/SSHTerminal.js
@@ -41,6 +41,7 @@ function SSHTerminal(auth, container) {
     t.connect = function(auth) {
         socket.emit('ssh:connect', auth, options, function(success) {
             t.emitter.emit('connect', success);
+            t.attach(container);
         });
     };
 
@@ -50,7 +51,6 @@ function SSHTerminal(auth, container) {
 
     socket.on('connect', function() {
         t.connect(auth);
-        t.attach(container);
     });
 
     xterm.on('data', function(data) {
