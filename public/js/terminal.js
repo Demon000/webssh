dc734e108241edec863197ac9be98cd8dcaeda44
Demon <demonsingur@gmail.com>
2018-10-02 09:39:28 +0000

    webssh: remove activity light handling
    Not exactly accurate.

--- public/js/terminal.js
+++ public/js/terminal.js
@@ -1,28 +1,4 @@
 (function() {
-    var handleActivity = false;
-    function setActivity(status) {
-        if (!handleActivity) {
-            return;
-        }
-
-        var favicon = document.querySelector('#favicon');
-        if (status) {
-            favicon.href = '/assets/activity.png';
-        } else {
-            favicon.href = '';
-        }
-    }
-
-    function handleVisibilityChange() {
-        if (document.hidden) {
-            handleActivity = true;
-        } else  {
-            setActivity(false);
-            handleActivity = false;
-        }
-    }
-    document.addEventListener('visibilitychange', handleVisibilityChange);
-
     var terminalContainer = document.querySelector('#terminal');
     var terminal = new SSHTerminal(terminalContainer);
     terminal.setOption('theme', {
@@ -40,8 +16,6 @@
                 break;
         }
     }
-
-    terminal.emitter.on('activity', setActivity);
 
     terminal.emitter.on('connect', function() {
         setConnectionStatus('connect');
